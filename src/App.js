421271edca02ab626b0c7f94744e1b857e3c5c7d
Wang Hao <991060@gmail.com>
2024-11-27 20:19:33 +0800

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { config, passport } from '@imtbl/sdk'
+import { useAccount, WagmiProvider, createConfig, http, useConnect, useDisconnect } from 'wagmi'
+import { immutableZkEvmTestnet } from 'wagmi/chains'
+import { injected } from 'wagmi/connectors'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+export const passportInstance = new passport.Passport({
+  baseConfig: {
+    environment: config.Environment.SANDBOX,
+    publishableKey: process.env.REACT_APP_IMMUTABLE_PUBLISHABLE_KEY || '',
+  },
+  clientId: process.env.REACT_APP_IMMUTABLE_CLIENT_ID || 'CLIENT_ID',
+  redirectUri: 'http://localhost:3000/redirect',
+  logoutRedirectUri: 'http://localhost:3000/logout',
+  audience: 'platform_api',
+  scope: 'openid offline_access email transact',
+  popupOverlayOptions: {
+    disableGenericPopupOverlay: false,
+    disableBlockedPopupOverlay: false,
+  },
+  logoutMode: 'silent',
+})
+
+const queryClient = new QueryClient()
+
+const wagmiConfig = createConfig({
+  chains: [immutableZkEvmTestnet],
+  connectors: [injected()],
+  transports: {
+    [immutableZkEvmTestnet.id]: http(),
+  },
+})
+
+//==========================================================
+
+function Content() {
+  passportInstance.connectEvm()
+
+  const { address } = useAccount()
+  const { connectors, connect } = useConnect()
+  const { disconnect } = useDisconnect()
+
+  async function login() {
+    for (const connector of connectors) {
+      if (connector.name.includes('Immutable Passport')) {
+        connect({ connector })
+      }
+    }
+  }
+
+  async function logout() {
+    disconnect()
+    await passportInstance.logout()
+    alert('Logged out')
+  }
+
+  return (
+  <div>
+    <button onClick={login}>Login</button><br/>
+    <button onClick={logout}>Logout</button><br/>
+    <p>{address ? `Connected as ${address}` : 'Not logged in'}</p>
+  </div>)
+}
+
+//==========================================================
 
 function App() {
+  if (window.location.pathname === '/redirect') {
+    passportInstance.loginCallback()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+      <div className="App">
+        {!process.env.REACT_APP_IMMUTABLE_PUBLISHABLE_KEY && <p>Please set REACT_APP_IMMUTABLE_PUBLISHABLE_KEY in your .env file</p>}
+        {!process.env.REACT_APP_IMMUTABLE_CLIENT_ID && <p>Please set REACT_APP_IMMUTABLE_CLIENT_ID in your .env file</p>}
+        <header className="App-header">
+          <Content />
+        </header>
+      </div>
+      </QueryClientProvider>      
+    </WagmiProvider>
+    
+  )
 }
 
 export default App;
